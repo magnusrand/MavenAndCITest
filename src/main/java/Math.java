40b556831ea61562480352cfce1e8859bd238e40
Magnus Rand <magnus.rand@gmail.com>
2019-11-12 17:36:42 +1300

    Add project and Maven integrations

---
+++ src/main/java/Math.java
@@ -0,0 +1,10 @@
+public class Math {
+
+    public static int max(int a, int b){
+        return a >= b ? a : b;
+    }
+
+    public static int min(int a, int b){
+        return a <= b ? a : b;
+    }
+}
