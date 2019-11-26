6dde1ab44344f25672f9721a5e65f3b8e661daec
Magnus Rand <magnus.rand@gmail.com>
2019-11-27 11:52:05 +1300

    Add pow to class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(Math.max(2,5));
+        System.out.println(Math.pow(2,5));
+        System.out.println(Math.pow(2,0));
     }
 }
--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -7,4 +7,12 @@ public class Math {
     public static int min(int a, int b){
         return a <= b ? a : b;
     }
+
+    public static int pow(int a, int b) {
+        int result = 1;
+        for(int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
 }
